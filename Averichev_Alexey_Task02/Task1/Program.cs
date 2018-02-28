252a9c6ad12faec36c63a82496613317e563c6c8
Алексей Аверичев <a.averichev@gmail.com>
2018-02-28 13:59:22 +0300

    Набросок

--- Averichev_Alexey_Task02/Task1/Program.cs
+++ Averichev_Alexey_Task02/Task1/Program.cs
@@ -13,10 +13,29 @@ namespace Task1
             Console.Write($"Input {arg} value: ");
             return int.Parse(Console.ReadLine());
         }
+        static int InputArg2(string arg)
+        {
+            int result;
+            string s;
+
+            do
+            {
+                do
+                {
+                    Console.Write($"Input {arg} value: ");
+                    s = Console.ReadLine();
+                }
+                while (string.IsNullOrEmpty(s));
+                result = int.Parse(s);
+            }
+            while (result <= 0);
+            return result;
+        }
         static void Sqr()
         {
-            int argA = InputArg("A");
-            int argB = InputArg("B");
+
+            int argA = InputArg2("A");
+            int argB = InputArg2("B");
             int sqr = argA * argB;
             Console.WriteLine($"Square = {sqr}");
             Console.Write("Press any key...");
