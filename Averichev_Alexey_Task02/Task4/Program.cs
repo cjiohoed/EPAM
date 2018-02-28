01a86091c4cc28eb40437b8ad2178882fc5b6994
Алексей Аверичев <a.averichev@gmail.com>
2018-02-28 17:49:41 +0300

    task3-4

---
+++ Averichev_Alexey_Task02/Task4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void PrintMeth(int value, string symb)
+        {
+            for (int i = 0; i < value; i++)
+            {
+                Console.Write(symb);
+            }
+        }
+        static void PrintTrigon(int value)
+        {
+            for (int s = 1; s <= value; s++)
+            {
+                int stars = 2 * s - 1;
+                int spaces = value - s;
+                PrintMeth(spaces, "+");
+                PrintMeth(stars, "*");
+                PrintMeth(spaces, "+");
+                Console.WriteLine();
+            }
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+        static void Main(string[] args)
+        {
+            int str = 20;
+            PrintTrigon(str);
+        }
+    }
+}
