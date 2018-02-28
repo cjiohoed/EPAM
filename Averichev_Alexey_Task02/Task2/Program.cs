a88010ac173821332eb42e0cbab7d4c8b21ad517
Алексей Аверичев <a.averichev@gmail.com>
2018-02-28 15:55:03 +0300

    new

---
+++ Averichev_Alexey_Task02/Task2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void StarLine(int value)
+        {
+            for (int i = 0; i < value; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        static void StarLines(int value)
+        {
+            for (int i = 1; i <= value; i++)
+            {
+                StarLine(i);
+            }
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+        static int Input()
+        {
+            Console.Write("Input value of strings: ");
+            int s = int.Parse(Console.ReadLine());
+            return s;
+        }
+        static void Main(string[] args)
+        {
+            StarLines(Input());
+        }
+    }
+}
---
+++ Averichev_Alexey_Task02/Task3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static bool Div(int n, int d)
+        {
+            if (d != 0 && (n % d) == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            int maxValue = 1000;
+            int sum = 0;
+            for (int i = 1; i < maxValue; i++)
+            {
+                if (Div(i, 3) || Div(i, 5))
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.WriteLine($"Sum of numbers 1-{maxValue} dividing by 3 and 5: {sum}");
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
