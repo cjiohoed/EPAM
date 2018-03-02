771bb088c8d935bff3814c6cf235449349a250db
Алексей Аверичев <a.averichev@gmail.com>
2018-03-02 16:36:33 +0300

    Task 03.2

---
+++ Averichev_Alexey_Task03/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lib;
+using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayOfNumbers = new int[5,6,8];
+            Arr.CreateRandomThreeD(arrayOfNumbers, My.InputArgSimple("min element"), My.InputArgSimple("max element"));
+            Arr.PositiveToZero(arrayOfNumbers);
+            My.Goodbye();
+        }
+    }
+}
