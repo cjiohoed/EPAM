072094231bd548029b362991a033950a0ae748d3
Алексей Аверичев <a.averichev@gmail.com>
2018-03-02 15:07:49 +0300

    Task 03.1

---
+++ Averichev_Alexey_Task03/Lib/Class1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Lib
+{
+    public class My
+    {
+        public static int InputArg(string arg)
+        {
+            int result;
+            string s;
+            do
+            {
+                Console.Write($"Input {arg}: ");
+                s = Console.ReadLine();
+                if (string.IsNullOrEmpty(s))
+                {
+                    Console.Write("Value is null or empty! ");
+                }
+                else if (!int.TryParse(s, out result))
+                {
+                    Console.Write("Incorrect parse! ");
+                }
+                else if (result <= 0)
+                {
+                    Console.Write("Value can't be null or negative! ");
+                }
+            }
+            while (string.IsNullOrEmpty(s) || !int.TryParse(s, out result) || result <= 0);
+            return result;
+        }
+
+        public static int InputArgSimple(string arg)
+        {
+            int result;
+            string s;
+            do
+            {
+                Console.Write($"Input {arg}: ");
+                s = Console.ReadLine();
+                if (string.IsNullOrEmpty(s))
+                {
+                    Console.Write("Value is null or empty! ");
+                }
+                else if (!int.TryParse(s, out result))
+                {
+                    Console.Write("Incorrect parse! ");
+                }
+            }
+            while (string.IsNullOrEmpty(s) || !int.TryParse(s, out result));
+            return result;
+        }
+
+        public static void Goodbye()
+        {
+            Console.Write("Press any key...");
+            Console.ReadKey();
+        }
+    }
+    public class Arr
+    {
+        public static void CreateRandom(int[] arr, int min, int max)
+        {
+            Random r = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = r.Next(min, max);
+            }
+        }
+
+        public static void Display(int[] arr)
+        {
+            Console.WriteLine("Array elements:");
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"arr[{i}] = {arr[i]}");
+            }
+        }
+
+        public static int[] BubbleSort(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
+
+        public static int MaxValue(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+        public static int MinValue(int[] arr)
+        {
+            int max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < max)
+                {
+                    max = arr[i];
+                }
+            }
+            return max;
+        }
+    }
+}
---
+++ Averichev_Alexey_Task03/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using Lib;
+using System;
+
+namespace Task1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var arrayOfNumbers = new int[My.InputArg("elements of array")];
+            Arr.CreateRandom(arrayOfNumbers, My.InputArgSimple("min element"), My.InputArgSimple("max element"));
+            Arr.BubbleSort(arrayOfNumbers);
+            Arr.Display(arrayOfNumbers);
+            Console.WriteLine($"Min value of array: {Arr.MinValue(arrayOfNumbers)}");
+            Console.WriteLine($"Max value of array: {Arr.MaxValue(arrayOfNumbers)}");
+            My.Goodbye();
+        }
+    }
+}
