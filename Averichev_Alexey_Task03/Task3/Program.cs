ea9f9548aae0ef50fe2b867b1d3b0f9e70dd716e
Алексей Аверичев <a.averichev@gmail.com>
2018-03-02 17:27:49 +0300

    Task 03.3

---
+++ Averichev_Alexey_Task03/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿using Lib;
+using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arrayOfNumbers = new int[My.InputArg("elements of array")];
+            Arr.CreateRandom(arrayOfNumbers, My.InputArgSimple("min element"), My.InputArgSimple("max element"));
+            Console.WriteLine($"Summ of positive elements: {Arr.GetPositiveSum(arrayOfNumbers)}");
+            My.Goodbye();
+        }
+    }
+}
