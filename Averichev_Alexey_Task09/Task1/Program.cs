daadc97ccf7289ef09b9d6c3ac06a28a5038d5bb
Алексей Аверичев <a.averichev@gmail.com>
2018-03-26 18:08:46 +0300

    next

--- Averichev_Alexey_Task09/Task1/Program.cs
+++ Averichev_Alexey_Task09/Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,26 @@ namespace Task1
     {
         static void Main()
         {
+
+            //var names = new[] { "Вова", "Вася", "Маша", "Сережа", "Жора", "Света", "Марина" };
+            var names = CreateNames(100);
+            var list1 = new List<string>(names);
+            var list2 = new LinkedList<string>(names);
+
+
         }
+
+        static string[] CreateNames(int count)
+        {
+            var arr = new string[count];
+            for (int i = 0; i < count; i++)
+            {
+                arr[i] = (i + 1).ToString();
+            }
+            return arr;
+        }
+
     }
+
+
 }
