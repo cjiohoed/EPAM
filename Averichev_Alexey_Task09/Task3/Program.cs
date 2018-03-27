f44165ea37f2e832ffa6defeb03c6c5b4376635d
Алексей Аверичев <a.averichev@gmail.com>
2018-03-27 17:28:59 +0300

    Task 09.3 done

---
+++ Averichev_Alexey_Task09/Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main()
+        {
+            var text = "Lorem ipsum dolor sit amet, " +
+                "consectetur adipiscing elit, sed do eiusmod tempor incididunt " +
+                "ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " +
+                "nostrud exercitation ullamco laboris nisi ut aliquip ex ea " +
+                "commodo consequat. Duis aute irure dolor in reprehenderit in " +
+                "voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                "Excepteur sint occaecat cupidatat non proident, sunt in culpa " +
+                "qui officia deserunt mollit anim id est laborum";
+
+            var dict = new WordsCounter(text);
+
+            foreach (var item in dict.Dictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            var a = dict.Text;
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Averichev_Alexey_Task09/Task3/WordsCounter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Task3
+{
+    public class WordsCounter
+    {
+        public string Text { get; }
+        public Dictionary<string, int> Dictionary
+        {
+            get
+            {
+                CreateDictionary(words);
+                CountWords(words);
+                return dict;
+            }
+        }
+
+        private Dictionary<string, int> dict = new Dictionary<string, int>();
+        private string[] words;
+
+        public WordsCounter(string text)
+        {
+            Text = text;
+            words = SplitWords(Text);
+        }
+
+        public string[] SplitWords(string text)
+        {
+            const string pattern = @"\W+";
+            return Regex.Split(text, pattern, RegexOptions.IgnoreCase);
+        }
+
+        private void CountWords(string[] words)
+        {
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (dict.ContainsKey(words[i]))
+                {
+                    dict[words[i]]++;
+                }
+            }
+        }
+
+        private void CreateDictionary(string[] words)
+        {
+            for (var i = 0; i < words.Length; i++)
+            {
+                if (dict.ContainsKey(words[i]))
+                {
+                    continue;
+                }
+                dict.Add(words[i], 0);
+            }
+        }
+    }
+}
