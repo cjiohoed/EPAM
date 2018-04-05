fc69f057e8bb52b8fe15345e25f622f1c282e61a
Алексей Аверичев <a.averichev@gmail.com>
2018-04-05 18:10:19 +0300

    Task 11.3

--- Averichev_Alexey_Task11/Task3/Program.cs
+++ Averichev_Alexey_Task11/Task3/Program.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
 
 namespace Task3
 {
@@ -10,11 +10,19 @@ namespace Task3
     {
         static void Main(string[] args)
         {
+            var a = new TwoDPointWithHash(1, 1);
+            var b = new TwoDPointWithHash(10, 10);
+
+            var c = new TwoDPointWithHash(1, 10);
+            var d = new TwoDPointWithHash(10, 1);
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            Console.WriteLine($"{c} =equals= {d}\t{c.Equals(d).ToString()}");
+            Console.WriteLine($"{c} =hash= {d}\t{(c.GetHashCode() == d.GetHashCode()).ToString()}");
+
+            Console.ReadKey();
         }
-    }
-
-    public class TwoDPointWithHash
-    {
-
     }
 }
---
+++ Averichev_Alexey_Task11/Task3/TwoDPointWithHash.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task3
+{
+    public class TwoDPointWithHash : IEquatable<TwoDPointWithHash>
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+
+        public TwoDPointWithHash(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public bool Equals(TwoDPointWithHash other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, other))
+            {
+                return true;
+            }
+
+            return X == other.X & Y == other.Y;
+
+        }
+
+        public override int GetHashCode()
+        {
+            //return unchecked(X.GetHashCode() ^ Y.GetHashCode());
+
+            unchecked
+            {
+                int hashCode = 0;
+                hashCode = (hashCode * 397) ^ X.GetHashCode();
+                hashCode = (hashCode * 397) ^ Y.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"({X};{Y})";
+        }
+    }
+}
