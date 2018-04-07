3749208b6d6547c2d917a470d0564be38b68a5a4
Алексей Аверичев <a.averichev@gmail.com>
2018-04-07 04:37:33 +0300

    12.1 done

---
+++ Averichev_Alexey_Task12/Task1/FileDataReplace.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace Task1
+{
+    public class FileDataReplace
+    {
+        private DirectoryInfo dir;
+        private FileInfo file;
+
+        private int count;
+        private int[] array;
+
+        public delegate int Func(int x);
+        private Func func;
+
+        public delegate void Notification();
+        public event Notification ReplaceDone;
+
+        public DirectoryInfo Directory
+        {
+            get
+            {
+                return dir;
+            }
+
+            set
+            {
+                if (!value.Exists)
+                {
+                    throw new Exception("Directory not exists");
+                }
+
+                dir = value;
+            }
+        }
+
+        public FileInfo FileName
+        {
+            get
+            {
+                return file;
+            }
+
+            set
+            {
+                if (!value.Exists)
+                {
+                    throw new Exception("File not exists");
+                }
+
+                file = value;
+            }
+        }
+
+        public FileDataReplace(
+            string dir,
+            string name,
+            Func modificator)
+        {
+            Directory = new DirectoryInfo(dir);
+            var path = Path.Combine(Directory.FullName, name);
+            FileName = new FileInfo(path);
+            func = modificator;
+        }
+
+        private void ReadFile()
+        {
+            count = File.ReadAllLines(FileName.ToString()).Length;
+            array = new int[count];
+
+            using (StreamReader sr = FileName.OpenText())
+            {
+                string fileContent;
+                int num;
+
+                for (var i = 0; i < count; i++)
+                {
+                    fileContent = sr.ReadLine();
+
+                    if (!int.TryParse(fileContent, out num))
+                    {
+                        array[i] = 0;
+                        continue;
+                    }
+
+                    array[i] = num;
+                }
+            }
+        }
+
+        private void WriteFile()
+        {
+            using (StreamWriter sw = FileName.CreateText())
+            {
+                for (var i = 0; i < count; i++)
+                {
+                    sw.WriteLine(array[i]);
+                }
+            }
+        }
+
+        private void ModifyData()
+        {
+            for (var i = 0; i < count; i++)
+            {
+                array[i] = func(array[i]);
+            }
+        }
+
+        public void Replace()
+        {
+            ThreadStart ReplaceInThread = () =>
+            {
+                ReadFile();
+                ModifyData();
+                WriteFile();
+
+                ReplaceDone?.Invoke();
+            };
+
+            Thread thread = new Thread(ReplaceInThread);
+
+            thread.Start();
+        }
+
+        public override string ToString()
+        {
+            if (array == null)
+            {
+                ReadFile();
+            }
+
+            var result = new StringBuilder();
+
+            for (var i = 0; i < count; i++)
+            {
+                result.Append(array[i]);
+                result.Append("\n");
+            }
+
+            return result.ToString();
+        }
+    }
+}
--- Averichev_Alexey_Task12/Task1/Program.cs
+++ Averichev_Alexey_Task12/Task1/Program.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace Task1
 {
@@ -11,81 +9,16 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-
-            DirectoryInfo dir = new DirectoryInfo(@"D:\C#\Tasks\Task12");
+            var dir = @"D:\C#\Tasks\Task12";
             var fileName = "disposable_task_file.txt";
-            string filePath = Path.Combine(dir.FullName, fileName);
-            FileInfo file = new FileInfo(filePath);
+            int func(int x) => x * x;
 
-            if (!file.Exists)
-            {
-                throw new Exception("File not exists");
-            }
+            var test = new FileDataReplace(dir, fileName, func);
+            test.ReplaceDone += () => Console.WriteLine("Done!");
+            test.Replace();
+            Console.WriteLine(test);
 
-            var list = new List<int>();
-
-            // строк в файле
-            int count = File.ReadAllLines(filePath).Length;
-
-            // готовим массив
-            var array = new int[count];
-
-            using (StreamReader sr = file.OpenText())
-            {
-                string fileContent;
-                int num;
-
-                for(var i = 0; i < count; i++)
-                {
-                    fileContent = sr.ReadLine();
-
-                    if (!int.TryParse(fileContent, out num))
-                    {
-                        array[i] = 0;
-                        continue;
-                    }
-
-                    array[i] = num * num;
-
-                    Console.WriteLine(array[i]);
-                }
-            }
-
-            Console.Write("\nPress any key...");
             Console.ReadKey();
-        }
-    }
-
-    public class FileClass : IDisposable
-    {
-        private readonly StreamWriter fileStream;
-
-        public string FileName
-        {
-            get
-            {
-                return FileName;
-            }
-            private set
-            {
-                if (String.IsNullOrEmpty(value))
-                {
-                    throw new Exception("File is null or empty");
-                }
-            }
-        }
-
-        public FileClass(string filename)
-        {
-            FileName = filename;
-        }
-
-        public new void Dispose()
-        {
-            if (fileStream != null)
-            {
-                fileStream.Dispose();
-            }
         }
     }
 }
